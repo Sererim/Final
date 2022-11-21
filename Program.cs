ac48f75a4543a6593ff223a0ae4fef3944fbf38c
Sererim <danielleymanw@gmail.com>
2022-11-21 17:56:56 +0300

    Added and tested class Utils for handling repeating parts of Main fucntion.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int Main()
+{
+    string[] input = new string[4] {"hello", "2", "world", ":-)"};
+    int num = 3;
+    string control = "50";
+    num = Utils.StringToInt(control);
+    Console.WriteLine(num);
+
+    Utils.ShowArray(4, input);
+
+    return 0;
+}
+
+Main();
+
+class Utils
+{
+    static public int StringToInt(string foo)
+    {
+        return System.Int32.Parse(foo);
+    }
+
+    static public void ShowArray(int size, string[] str)
+    {
+        for(int foo = 0; foo < size; foo++)
+            Console.WriteLine($"content: {str[foo]} - position: {foo}");
+    }
+}
+
+class Solution
+{
+
+}
